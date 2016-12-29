0cacd6b17414120a09cc478f9de7fc15511ae789
laptopsr <laptopsr@gmail.com>
2016-12-29 22:32:43 +0200

    Omat kansiot

--- protected/views/site/oma_kansio.php
+++ protected/views/site/oma_kansio.php
@@ -1,10 +1,34 @@
 <?php
 
+			// <-- Onko tiedosto
+			if(isset($_FILES['fileUpload']) and !empty($_FILES['fileUpload']))
+			{
+				$img_desc = $this->reArrayFiles($_FILES['fileUpload']);
+
+				$path = Yii::app()->basePath."/../";
+				$nextPath = $_POST['kansionSijainti'];
+				if (!file_exists($path . $nextPath)) {
+					mkdir($path . $nextPath, 0777, true);
+				}
+    				$files = array();
+				$onkokuva = false;
+    				foreach($img_desc as $val)
+    				{
+					$uploaddir = $path . $nextPath. '/';
+					$uploadfile = basename($val['name']);	
+					if (move_uploaded_file($val['tmp_name'], $uploaddir . $uploadfile)) {
+
+					}
+    				}
+
+			}
+			//  Onko tiedosto -->
+
 
 
 ?>
  
- <legend><h2>Omat tiedostot</h2></legend>
+ <h2>Omat tiedostot</h2>
 
 
  <div id="result"></div>
@@ -14,27 +38,97 @@
 $(document).ready(function(){
 
 
- openAjaxTiedosto();
+ openAjaxTiedosto(null);
 
- function openAjaxTiedosto(){
+ function openAjaxTiedosto(variables){
 
 	var yid = $('#yid').val();
 
         $.ajax({
           type: 'POST',
           url: 'oma_kansio?yid='+yid,
-          data: { ajax : "true" },
+          data: { ajax : "true", variables : variables },
           success: function(data) {
 		data=JSON.parse(data);
-		//console.log(data);
+		//console.log(variables);
 		$('#result').html(data);
+
+
+		if((variables !== null) && (variables[0]['paaKansio'])){
+			$('.nav.nav-tabs').find('.active').removeClass('active');
+			$('#paaKansio').addClass('active');
+		}
+		if((variables !== null) && (variables[0]['luoKansio'])){
+			$('.nav.nav-tabs').find('.active').removeClass('active');
+			$('#luoKansio').addClass('active');
+			$('#luoKansioLomake').show(370);
+		}
+		if((variables !== null) && (variables[0]['lataaTiedosto'])){
+			$('.nav.nav-tabs').find('.active').removeClass('active');
+			$('#lataaTiedosto').addClass('active');
+			$('#lataaTiedostoLomake').show(370);
+		}
           },
           error:  function(xhr, str){
-	    alert('error: ' + xhr.responseCode);
+	    console.log('error: ' + xhr.responseCode);
           }
         });
 
  };
 
+
+ $(document).delegate("#paaKansio","click",function(){
+	var variables = [{'paaKansio': 'true'}];
+	openAjaxTiedosto(variables);
+ });
+
+ $(document).delegate("#luoKansio","click",function(){
+	var variables = [{'luoKansio': 'true'}];
+	openAjaxTiedosto(variables);
+ });
+
+ $(document).delegate(".luoUusiKansioSubmit","click",function(){
+	var kansionNimi 	= $('#kansionNimi').val();
+	var kansionSijainti 	= $('#kansionSijainti').val();
+
+	var variables = [{'kansionNimi': kansionNimi, 'kansionSijainti': kansionSijainti}];
+	openAjaxTiedosto(variables);
+ });
+
+ $(document).delegate("#lataaTiedosto","click",function(){
+	var variables = [{'lataaTiedosto': 'true'}];
+	openAjaxTiedosto(variables);
+ });
+
+ $(document).delegate(".lataaTiedostoSubmit","click",function(){
+	$('#uusiTiedostoForm').submit();
+ });
+
+ $(document).delegate(".poista","click",function(){
+	var pois = $(this).attr('pois');
+	var variables = [{'poista': pois}];
+	if(confirm('Haluatko poista tiedoston?'))
+	openAjaxTiedosto(variables);
+ });
+
+ $(document).delegate(".poistaKansio","click",function(){
+	var pois = $(this).attr('pois');
+	var variables = [{'poistaKansio': pois}];
+	if(confirm('Haluatko poista kansion?')){
+		openAjaxTiedosto(variables);
+		window.location.href="oma_kansio";
+	}
+ });
+
+
 });
 </script>
+
+
+
+
+
+
+
+
+
--- protected/views/site/oma_kansio_ajax.php
+++ protected/views/site/oma_kansio_ajax.php
@@ -5,40 +5,185 @@
  if (!file_exists($path . $nextPath)) {
  	mkdir($path . $nextPath, 0777, true);
  }
- chmod($path . $nextPath, 755);
+ if(isset($_POST['variables'][0]['kansionNimi']) and $_POST['variables'][0]['kansionNimi'] != ''){
+	$uusikansio = $path.$_POST['variables'][0]['kansionSijainti'].'/'.$_POST['variables'][0]['kansionNimi'];
+ 	if (!file_exists($uusikansio)) {
+ 		mkdir($uusikansio, 0777, true);
+ 	}
+ }
+
+ if(isset($_POST['variables'][0]['poista']) and $_POST['variables'][0]['poista'] != ''){
+
+ 	if (file_exists($_POST['variables'][0]['poista'])) {
+ 		unlink($_POST['variables'][0]['poista']);
+ 	}
+ }
+
+ if(isset($_POST['variables'][0]['poistaKansio']) and $_POST['variables'][0]['poistaKansio'] != ''){
+
+
+	function deleteDirectory($dirPath) {
+	    if (is_dir($dirPath)) {
+	        $objects = scandir($dirPath);
+	        foreach ($objects as $object) {
+	            if ($object != "." && $object !="..") {
+	                if (filetype($dirPath . DIRECTORY_SEPARATOR . $object) == "dir") {
+	                    deleteDirectory($dirPath . DIRECTORY_SEPARATOR . $object);
+	                } else {
+	                    unlink($dirPath . DIRECTORY_SEPARATOR . $object);
+	                }
+	            }
+	        }
+	    reset($objects);
+	    rmdir($dirPath);
+	    }
+	}
+
+	deleteDirectory($_POST['variables'][0]['poistaKansio']);
+ }
 ?>
 
 <ul class="nav nav-tabs">
-  <li role="presentation" class="active"><a href="#">Pääkansio</a></li>
-  <li role="presentation"><a href="#">Lataa tiedosto</a></li>
-  <li role="presentation"><a href="#">Muu</a></li>
+  <li role="presentation" class="active" id="paaKansio"><a href="#">Kotiin</a></li>
+  <li role="presentation" id="luoKansio"><a href="#">Luo kansio</a></li>
+  <li role="presentation" id="lataaTiedosto"><a href="#">Lataa tiedosto</a></li>
 </ul>
+
+<br>
+
+<!-- Luo kansio -->
+<?php if(isset($_POST['variables'][0]['luoKansio'])) : ?>
+<div class="row" id="luoKansioLomake" style="display:none">
+ <div class="col-sm-4">
+
+<div class="panel panel-default">
+  <!-- Default panel contents -->
+  <div class="panel-heading">Uusi kansio</div>
+  <div class="panel-body">
+    <p class="form row">
+    <div class="col-sm-12">
+
+     <div class="row">
+  	<label>Kansion nimi</label>
+	<input type="text" id="kansionNimi" class="form-control">
+     </div>
+
+     <div class="row">
+  	<label>Mihin alle luodaan kansio</label>
+	<select id="kansionSijainti" class="form-control">
+	<option value="<?php echo $nextPath; ?>">Pääkansio</option>
+	<?php
+	  function naytaKansiot($folder, $space) {
+	    $files = scandir($folder);
+	    foreach($files as $file) {
+	      if (($file == '.') || ($file == '..')) continue;
+	      $f0 = $folder.'/'.$file;
+	
+	      if (is_dir($f0)) {
+	
+	        echo '<option value="'.$f0.'">'.$file.'</option>';
+	
+	        naytaKansiot($f0, $space.'&nbsp;&nbsp;');
+	      }
+	    }
+	  }
+	  naytaKansiot($nextPath, "");
+	?>
+	</select>
+     </div>
+
+     <br>
+     <div class="row">
+	<button class="btn btn-primary luoUusiKansioSubmit">Luo</button>
+     </div>
+    </div>
+    </p>
+  </div>
+</div>
+
+
+ </div>
+</div>
+<?php endif; ?>
+<!-- Luo kansio -->
+
+
+
+<!-- Lataa tiedosto -->
+<?php if(isset($_POST['variables'][0]['lataaTiedosto'])) : ?>
+<div class="row" id="lataaTiedostoLomake" style="display:none">
+ <div class="col-sm-4">
+
+<div class="panel panel-default">
+  <!-- Default panel contents -->
+  <div class="panel-heading">Lataa tiedosto</div>
+  <div class="panel-body">
+    <p class="form row">
+    <div class="col-sm-12">
+
+     <form id="uusiTiedostoForm" action="oma_kansio" enctype="multipart/form-data" method="POST">
+     <div class="row">
+  	<label>Mihin kansioon</label>
+	<select name="kansionSijainti" class="form-control">
+	<option value="<?php echo $nextPath; ?>">Pääkansio</option>
+	<?php
+	  function naytaKansiot($folder, $space) {
+	    $files = scandir($folder);
+	    foreach($files as $file) {
+	      if (($file == '.') || ($file == '..')) continue;
+	      $f0 = $folder.'/'.$file;
+	
+	      if (is_dir($f0)) {
+	
+	        echo '<option value="'.$f0.'">'.$file.'</option>';
+	
+	        naytaKansiot($f0, $space.'&nbsp;&nbsp;');
+	      }
+	    }
+	  }
+	  naytaKansiot($nextPath, "");
+	?>
+	</select>
+     </div>
+
+     <div class="row">
+  	<label>Tiedosto</label>
+	<input type="file" name="fileUpload[]" multiple id="fileUpload" class="form-control">
+     </div>
+     </form>
+
+     <br>
+     <div class="row">
+	<button class="btn btn-primary lataaTiedostoSubmit">Luo</button>
+     </div>
+    </div>
+    </p>
+  </div>
+</div>
+
+
+ </div>
+</div>
+<?php endif; ?>
+<!-- Lataa tiedosto -->
 
 
 <?php
 echo '<pre>';
 
   function showTree($folder, $space) {
-    /* Получаем полный список файлов и каталогов внутри $folder */
     $files = scandir($folder);
     foreach($files as $file) {
-      /* Отбрасываем текущий и родительский каталог */
       if (($file == '.') || ($file == '..')) continue;
-      $f0 = $folder.'/'.$file; //Получаем полный путь к файлу
-      /* Если это директория */
+      $f0 = $folder.'/'.$file;
       if (is_dir($f0)) {
-        /* Выводим, делая заданный отступ, название директории */
-        echo '<i class="fa fa-folder-open" aria-hidden="true"></i> '.$space.$file."<br />";
-        /* С помощью рекурсии выводим содержимое полученной директории */
+        echo $space.'<i class="fa fa-times link poistaKansio" pois="'.$folder.'/'.$file.'" aria-hidden="true" style="font-size:120%;color: red;"></i> <i class="fa fa-folder-open" aria-hidden="true" style="font-size:120%;color: #ffa31a;"></i> '.$file."<br />";
         showTree($f0, $space.'&nbsp;&nbsp;');
-      }
-      /* Если это файл, то просто выводим название файла */
-      else {
-	echo $space.'<a href="../../'.$folder.'/'.$file.'"><i class="fa fa-file" aria-hidden="true"></i> '.$file.'</a><br />';
+      } else {
+	echo $space.'<i class="fa fa-times link poista" pois="'.$folder.'/'.$file.'" aria-hidden="true" style="font-size:120%;color: red;"></i> <i class="fa fa-file" aria-hidden="true" style="font-size:120%;color: #ccc;"></i> <a href="../../'.$folder.'/'.$file.'">'.$file.'</a><br />';
       }
     }
   }
-  /* Запускаем функцию для текущего каталога */
   showTree($nextPath, "");
 
 echo '</pre>';
